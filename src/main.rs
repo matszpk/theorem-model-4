fe566c983d87bf90d9ab24201fbd140e82703524
matszpk <matszpk@interia.pl>
2023-02-25 12:54:36 +0100

    Fix and debug printlns.

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ impl PrimalMachine {
                         let sc_output = self.run_circuit(Some(sc), &sc_input, level + 1);
 
                         for i in 0..sc_output_len {
-                            let v = sc_output[i >> 3] >> (i & 7) != 0;
+                            let v = (sc_output[i >> 3] >> (i & 7)) & 1 != 0;
                             next_step_mem[(oi + i) >> 3] |= u8::from(v) << ((oi + i) & 7);
                         }
 
@@ -102,9 +102,24 @@ impl PrimalMachine {
                     }
                 }
             }
+            println!("OI {}", oi);
+            println!(
+                "  OldValues: {:?}",
+                (0..16)
+                    .map(|i| { format!("{}:{}", i, (next_step_mem[i >> 3] >> (i & 7)) & 1) })
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            );
+            println!(
+                "  OldValues: {:?}",
+                (0..16)
+                    .map(|i| { format!("{}:{}", i, (step_mem[i >> 3] >> (i & 7)) & 1) })
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            );
             // copy previous to next step
             for i in 0..128 - oi {
-                let v = step_mem[i >> 3] >> (i & 7) != 0;
+                let v = (step_mem[i >> 3] >> (i & 7)) & 1 != 0;
                 next_step_mem[(oi + i) >> 3] |= u8::from(v) << ((oi + i) & 7);
             }
 
@@ -131,10 +146,36 @@ impl PrimalMachine {
 fn main() {
     let mut pm = PrimalMachine::new();
     pm.circuit.extend([
-        2, 1, 1, 0, 0, 2, 0xff, 0, 1, 1, 3, 0, 5, 0xff, 5, 0, 2, 1, 0xff, 7, 0, 9, 0, 0xff, 11, 3,
-        0xff, 1, 0, 0xff,
+        // machine.put_nand(t0 [0], a2 [2], a1 [1]);
+        // machine.put_nand(t1 [1], a1 [1], a0 [0]);
+        // machine.put_nand(t2 [2], a0 [0], a2 [2]);
+        2, 1, 1, 0, 0, 2, 0xff,
+        // a0 [3], a1 [4], a2 [5]
+        // machine.put_nand(t3 [0], t0 [0], t1 [1]);
+        // machine.put_nand(t4 [1], t1 [1], a0 [3]);
+        // machine.put_nand(t5 [2], t0 [0], a2 [5]);
+        0, 1, 1, 3, 0, 5, 0xff,
+        // t0 [3], t1 [4], t2 [5], a0 [6], a1 [7], a2 [8]
+        // machine.put_nand(t6 [0], t2 [5], t3 [0]);
+        // machine.put_nand(t7 [1], t5 [2], t4 [1]);
+        5, 0, 2, 1, 0xff,
+        // t3 [2], t4 [3], t5 [4], t0 [5], t1 [6], t2 [7], a0 [8], a1 [9], a2 [10]
+        // machine.put_nand(t8 [0], t2 [7], t6 [0]);
+        // machine.put_nand(t9 [1], a1 [9], t6 [0]);
+        7, 0, 9, 0, 0xff,
+        // t6 [2], t7 [3], t3 [4], t4 [5], t5 [6], t0 [7], t1 [8], t2 [9],
+        // a0 [10], a1 [11], a2 [12]
+        // machine.put_nand(t10 [0], t2 [9], t7 [3]);
+        9, 3, 0xff,
+        // t8 [1], t9 [2], t6 [3], t7 [4], t3 [5], t4 [6], t5 [7], t0 [8], t1 [9], t2 [10],
+        // a0 [11], a1 [12], a2 [13]
+        // machine.put_nand(r0 [0], t9 [2], t10 [0]);
+        2,
+        0, // t10 [1] t8 [2], t9 [3], t6 [4], t7 [5], t3 [6], t4 [7], t5 [8], t0 [9],
+           // t1 [10], t2 [11], a0 [12], a1 [13], a2 [14]
     ]);
     for i in 0..8 {
-        println!("Output: {:?}", pm.run(&[i]));
+        println!("-------------");
+        println!("Output: {:b}: {:08b}", i, pm.run(&[i])[0].reverse_bits());
     }
 }
