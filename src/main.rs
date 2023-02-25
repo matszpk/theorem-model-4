28e20cc27f7be721335719960979bdb05052b8b3
matszpk <matszpk@interia.pl>
2023-02-25 14:36:47 +0100

    Change ordering in step_mem. Remove unnecessary next_step_mem.

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,16 @@ impl PrimalMachine {
         }
     }
 
-    fn run_circuit(&self, circuit: Option<usize>, input: &[u8], level: u8) -> [u8; 128 >> 3] {
+    fn run_circuit(
+        &self,
+        circuit: Option<usize>,
+        input: &[u8],
+        input_len: usize,
+        level: u8,
+    ) -> ([u8; 128 >> 3], usize) {
         assert!(level < 8);
         assert!(input.len() < 128);
         let mut step_mem: [u8; 128 >> 3] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        let mut next_step_mem: [u8; 128 >> 3] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         // initialize input
         step_mem[0..input.len()].copy_from_slice(input);
@@ -37,9 +42,8 @@ impl PrimalMachine {
                 self.subcircuits[0].0
             });
 
+        let mut oi = input_len; // output index
         while step_index < circuit_end {
-            let mut oi = 0; // output index
-
             let mut nand_arg1: Option<bool> = None;
             while step_index < circuit_end {
                 let lv = self.circuit[step_index];
@@ -50,9 +54,9 @@ impl PrimalMachine {
                     let v = (step_mem[ii >> 3] >> (ii & 7)) & 1 != 0;
                     if let Some(v1) = nand_arg1 {
                         println!("Step: {} {}: {} {} {}", step_index, oi, v1, v, !(v1 & v));
-                        next_step_mem[oi >> 3] |= u8::from(!(v1 & v)) << (oi & 7);
+                        step_mem[oi >> 3] |= u8::from(!(v1 & v)) << (oi & 7);
                         nand_arg1 = None;
-                        oi += 1;
+                        oi = (oi + 1) & 127;
                     } else {
                         nand_arg1 = Some(v);
                     }
@@ -60,122 +64,86 @@ impl PrimalMachine {
                 } else {
                     if let Some(v1) = nand_arg1 {
                         // if next argument not found then flush with 1
-                        println!(
-                            "Step: {} {}: {} {} {}",
-                            step_index,
-                            oi,
-                            v1,
-                            true,
-                            !(v1 & true)
-                        );
-                        next_step_mem[oi >> 3] |= u8::from(!(v1 & true)) << (oi & 7);
+                        println!("Step: {} {}: {} {} {}", step_index, oi, v1, true, !v1);
+                        step_mem[oi >> 3] |= u8::from(!(v1 & true)) << (oi & 7);
                         nand_arg1 = None;
-                        oi += 1;
+                        oi = (oi + 1) & 127;
                     }
-                    if lv < 255 {
-                        // subcircuit call
-                        let sc = (lv - 128) as usize;
-                        let mut sc_input: [u8; 128 >> 3] =
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                    // subcircuit call
+                    let sc = (lv - 128) as usize;
+                    let mut sc_input: [u8; 128 >> 3] =
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-                        let sc_input_len = self.subcircuits[sc].1 as usize;
-                        let sc_output_len = self.subcircuits[sc].2 as usize;
-                        for i in 0..sc_input_len {
-                            let ii = self.circuit[step_index + i] as usize;
-                            let v = (step_mem[ii >> 3] >> (ii & 7)) & 1 != 0;
-                            sc_input[i >> 3] |= u8::from(v) << (i & 7);
-                        }
-                        step_index += sc_input_len;
+                    let sc_input_len = self.subcircuits[sc].1 as usize;
+                    let sc_output_len = self.subcircuits[sc].2 as usize;
+                    for i in 0..sc_input_len {
+                        let ii = self.circuit[step_index + i] as usize;
+                        let v = (step_mem[ii >> 3] >> (ii & 7)) & 1 != 0;
+                        sc_input[i >> 3] |= u8::from(v) << (i & 7);
+                    }
+                    step_index += sc_input_len;
 
-                        let sc_output = self.run_circuit(Some(sc), &sc_input, level + 1);
+                    let (sc_output, sc_oi) =
+                        self.run_circuit(Some(sc), &sc_input, sc_input_len, level + 1);
+                    let mut sc_oi = (128 + sc_oi - sc_output_len) & 127;
 
-                        for i in 0..sc_output_len {
-                            let v = (sc_output[i >> 3] >> (i & 7)) & 1 != 0;
-                            next_step_mem[(oi + i) >> 3] |= u8::from(v) << ((oi + i) & 7);
-                        }
-
-                        oi += sc_output_len;
-                    } else {
-                        step_index += 1;
-                        // 255: end of list
-                        break;
+                    for _ in 0..sc_output_len {
+                        let v = (sc_output[sc_oi >> 3] >> (sc_oi & 7)) & 1 != 0;
+                        step_mem[oi >> 3] |= u8::from(v) << (oi & 7);
+                        oi = (oi + 1) & 127;
+                        sc_oi = (sc_oi + 1) & 127;
                     }
                 }
             }
-            println!("OI {}", oi);
-            println!(
-                "  OldValues: {:?}",
-                (0..16)
-                    .map(|i| { format!("{}:{}", i, (next_step_mem[i >> 3] >> (i & 7)) & 1) })
-                    .collect::<Vec<_>>()
-                    .join(" ")
-            );
-            println!(
-                "  OldValues: {:?}",
-                (0..16)
-                    .map(|i| { format!("{}:{}", i, (step_mem[i >> 3] >> (i & 7)) & 1) })
-                    .collect::<Vec<_>>()
-                    .join(" ")
-            );
-            // copy previous to next step
-            for i in 0..128 - oi {
-                let v = (step_mem[i >> 3] >> (i & 7)) & 1 != 0;
-                next_step_mem[(oi + i) >> 3] |= u8::from(v) << ((oi + i) & 7);
-            }
-
-            step_mem = next_step_mem;
-            next_step_mem.fill(0);
-            println!("Next step");
-            println!(
-                "  Values: {:?}",
-                (0..16)
-                    .map(|i| { format!("{}:{}", i, (step_mem[i >> 3] >> (i & 7)) & 1) })
-                    .collect::<Vec<_>>()
-                    .join(" ")
-            );
         }
 
-        step_mem
+        (step_mem, oi)
     }
 
-    pub fn run(&self, input: &[u8]) -> [u8; 128 >> 3] {
-        self.run_circuit(None, input, 0)
+    pub fn run(&self, input: &[u8], input_len: usize) -> ([u8; 128 >> 3], usize) {
+        self.run_circuit(None, input, input_len, 0)
     }
 }
 
 fn main() {
     let mut pm = PrimalMachine::new();
     pm.circuit.extend([
-        // machine.put_nand(t0 [0], a2 [2], a1 [1]);
-        // machine.put_nand(t1 [1], a1 [1], a0 [0]);
-        // machine.put_nand(t2 [2], a0 [0], a2 [2]);
-        2, 1, 1, 0, 0, 2, 0xff,
-        // a0 [3], a1 [4], a2 [5]
-        // machine.put_nand(t3 [0], t0 [0], t1 [1]);
-        // machine.put_nand(t4 [1], t1 [1], a0 [3]);
-        // machine.put_nand(t5 [2], t0 [0], a2 [5]);
-        0, 1, 1, 3, 0, 5, 0xff,
-        // t0 [3], t1 [4], t2 [5], a0 [6], a1 [7], a2 [8]
-        // machine.put_nand(t6 [0], t2 [5], t3 [0]);
-        // machine.put_nand(t7 [1], t5 [2], t4 [1]);
-        5, 0, 2, 1, 0xff,
-        // t3 [2], t4 [3], t5 [4], t0 [5], t1 [6], t2 [7], a0 [8], a1 [9], a2 [10]
-        // machine.put_nand(t9 [0], a1 [9], t6 [0]);
-        9, 0, 0xff,
-        // t6 [1], t7 [2], t3 [3], t4 [4], t5 [5], t0 [6], t1 [7], t2 [8],
-        // a0 [9], a1 [10], a2 [11]
-        // machine.put_nand(t8 [0], t2 [8], t6 [1]);
-        // machine.put_nand(t10 [1], t2 [8], t7 [2]);
-        8, 1, 8, 2, 0xff,
-        // t9 [2], t6 [3], t7 [4], t3 [5], t4 [6], t5 [7], t0 [8], t1 [9], t2 [10],
-        // a0 [11], a1 [12], a2 [13]
-        // machine.put_nand(r0 [0], t9 [2], t10 [1]);
-        // t8 [1], t10 [2] t8 [3], t9 [4], t6 [5], t7 [6], t3 [7], t4 [8], t5 [9], t0 [10],
-        // t1 [11], t2 [12], a0 [13], a1 [14], a2 [15]
-        2, 1,
+        // a0 [0], a1 [1], a2 [2]
+        // machine.put_nand(t0 [3], a2 [2], a1 [1]);
+        // machine.put_nand(t1 [4], a1 [1], a0 [0]);
+        // machine.put_nand(t2 [5], a0 [0], a2 [2]);
+        2, 1, 1, 0, 0, 2,
+        // a0 [0], a1 [1], a2 [2], t0 [3], t1 [4], t2 [5]
+        // machine.put_nand(t3 [6], t0 [3], t1 [4]);
+        // machine.put_nand(t4 [7], t1 [4], a0 [0]);
+        // machine.put_nand(t5 [8], t0 [3], a2 [2]);
+        3, 4, 4, 0, 3, 2,
+        // a0 [0], a1 [1], a2 [2], t0 [3], t1 [4], t2 [5], t3 [6], t4 [7], t5 [8]
+        // machine.put_nand(t6 [9], t2 [5], t3 [6]);
+        // machine.put_nand(t7 [10], t5 [8], t4 [7]);
+        5, 6, 8, 7,
+        // a0 [0], a1 [1], a2 [2], t0 [3], t1 [4], t2 [5], t3 [6], t4 [7], t5 [8],
+        // t6 [9], t7 [10]
+        // machine.put_nand(t9 [11], a1 [1], t6 [9]);
+        1, 9,
+        // a0 [0], a1 [1], a2 [2], t0 [3], t1 [4], t2 [5], t3 [6], t4 [7], t5 [8],
+        // t6 [9], t7 [10], t9 [11]
+        // machine.put_nand(t10 [12], t2 [5], t7 [10]);
+        5, 10,
+        // a0 [0], a1 [1], a2 [2], t0 [3], t1 [4], t2 [5], t3 [6], t4 [7], t5 [8],
+        // t6 [9], t7 [10], t9 [11], t10 [12]
+        // machine.put_nand(r0 [13], t9 [11], t10 [12]);
+        // machine.put_nand(t8 [14], t2 [5], t6 [9]);
+        11, 12, 5, 9,
     ]);
     for i in 0..8 {
         println!("-------------");
-        println!("Output: {:b}: {:08b}", i, pm.run(&[i])[0].reverse_bits());
+        let output = pm.run(&[i], 3).0;
+        println!(
+            "Output: {:03b}: {:08b} {:08b}",
+            i,
+            output[0].reverse_bits(),
+            output[1].reverse_bits(),
+        );
     }
 }
