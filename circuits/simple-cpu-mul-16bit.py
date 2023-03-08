9aa62ada14f79ab854d767330b04e815d4508781
matszpk <matszpk@interia.pl>
2023-03-08 20:11:40 +0100

    Add next examples for simple-cpu.

---
+++ circuits/simple-cpu-mul-16bit.py
@@ -0,0 +1,68 @@
+from simple_cpu_asm import *
+from sys import stdout
+
+ml = Memory()
+
+mul_a = 0xf0
+mul_b = 0xf4
+mul_c = 0xf8
+mul_t1 = 0xe0
+mul_t2 = 0xe8
+
+skip_add = 0
+for i in range(0,2):
+    ml.set_pc(0x0)
+    start = ml.pc
+    # copy mul_a to mul_t1
+    ml.lda(start)    # zero
+    for i in range(0,4):
+        ml.lda(mul_a+i)
+        ml.sta(mul_t1+i)
+    # copy mul_b to mul_t2
+    for i in range(0,4):
+        ml.lda(mul_b+i)
+        ml.sta(mul_t2+i)
+    
+    # main loop
+    loop = ml.pc
+    # mul_t2>>1, c
+    ml.clc()
+    ml.lda(mul_t2+3)
+    ml.ror()
+    ml.sta(mul_t2+3)
+    ml.lda(mul_t2+2)
+    ml.ror()
+    ml.sta(mul_t2+2)
+    ml.lda(mul_t2+1)
+    ml.ror()
+    ml.sta(mul_t2+1)
+    ml.lda(mul_t2)
+    ml.ror()
+    ml.sta(mul_t2)
+    # bne to do 
+    pc = ml.pc
+    ml.bcc(skip_add)
+    # add: mul_t1 to result mul_c
+    ml.clc()
+    for i in range(0,8):
+        ml.lda(mul_t1+i)
+        ml.adc(mul_c+i)
+        ml.sta(mul_c+i)
+    skip_add = ml.pc
+    # shift mul_t1
+    ml.clc()
+    for i in range(0,8):
+        ml.lda(mul_t1+i)
+        ml.rol()
+        ml.sta(mul_t1+i)
+    # check t2
+    ml.lda(mul_t2)
+    ml.ior(mul_t2+1)
+    ml.bne(loop)
+    ml.pul()
+
+ml.set_pc(mul_a)
+ml.word16(5521)
+ml.word16(18721)
+
+stdout.buffer.write(ml.dump())
--- circuits/simple-cpu-mul2.py
+++ circuits/simple-cpu-mul2.py
@@ -8,7 +8,6 @@ mul_b = 0xfa
 mul_c = 0xfc
 mul_t1 = 0xf0
 mul_t2 = 0xf4
-mul_t3 = 0xf6
 
 skip_add = 0
 for i in range(0,2):
@@ -19,25 +18,25 @@ for i in range(0,2):
     for i in range(0,2):
         ml.lda(mul_a+i)
         ml.sta(mul_t1+i)
-    # copy 1 to mul_t2
-    ml.lda(start)    # zero
-    ml.sta(mul_t2+1)
-    ml.lda(start+2)  # 1
+    # copy mul_b to mul_t2
+    ml.lda(mul_b)
     ml.sta(mul_t2)
+    ml.lda(mul_b+1)
+    ml.sta(mul_t2+1)
     
     # main loop
     loop = ml.pc
-    # mul_t2&mul_b
-    ml.lda(mul_t2)
-    ml.iand(mul_b)
-    ml.sta(mul_t3)
+    # mul_t2>>1, c
+    ml.clc()
     ml.lda(mul_t2+1)
-    ml.iand(mul_b+1)
-    ml.ior(mul_t3)
+    ml.ror()
+    ml.sta(mul_t2+1)
+    ml.lda(mul_t2)
+    ml.ror()
+    ml.sta(mul_t2)
     # bne to do 
     pc = ml.pc
-    ml.bne(pc+6)
-    ml.jmp(skip_add)
+    ml.bcc(skip_add)
     # add: mul_t1 to result mul_c
     ml.clc()
     for i in range(0,4):
@@ -51,14 +50,10 @@ for i in range(0,2):
         ml.lda(mul_t1+i)
         ml.rol()
         ml.sta(mul_t1+i)
-    # shift mul_t2
-    ml.clc()
-    for i in range(0,2):
-        ml.lda(mul_t2+i)
-        ml.rol()
-        ml.sta(mul_t2+i)
-    # check if t2 is zero (check if carry in last)
-    ml.bcc(loop)
+    # check t2
+    ml.lda(mul_t2)
+    ml.ior(mul_t2+1)
+    ml.bne(loop)
     ml.pul()
 
 ml.set_pc(mul_a)
