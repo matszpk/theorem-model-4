936cfc957e35f652e764f4a1d1376bd73167542d
matszpk <matszpk@interia.pl>
2023-03-14 15:02:43 +0100

    First example.

---
+++ circuits/simple2-cpu/example1.py
@@ -0,0 +1,19 @@
+from simple2_cpu_asm import *
+from sys import stdout
+
+ml = Memory()
+
+ml.set_pc(0x00)
+ml.clc()
+for i in range(0,4):
+    ml.lda(0x210+i)
+    ml.adc(0x214+i)
+    ml.sta(0x330+i)
+ml.spc(0x80)
+ml.set_pc(0x80)
+ml.byte(1)
+ml.set_pc(0x210)
+ml.word32(0x7771bb23)
+ml.word32(0x436981a1)
+
+stdout.buffer.write(ml.dump())
