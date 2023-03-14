839b67d81b7cb0a1aaf940d3ba459da738710c31
matszpk <matszpk@interia.pl>
2023-03-14 18:42:59 +0100

    Add new examples for simple2-cpu. Remove unnecessary circuit from simple2-cpu.

---
+++ circuits/simple2-cpu/create-example1.py
@@ -0,0 +1,45 @@
+from simple2_cpu_asm import *
+from sys import stdout
+
+ml = Memory()
+
+ml.set_pc(0x00)
+ml.byte(0x0)
+ml.byte(0x10)
+ml.byte(0x40)
+ml.byte(0x0)
+ml.byte(0xff)
+
+start = 0x10
+ml.set_pc(start)
+
+for i in range(0,3):
+    ml.lda(0x1+i)
+    ml.sta(0xffd+i)
+ml.spc(0)
+
+ml.lda(0)
+ml.sta(0xffe)
+ml.sta(0xfff)
+
+# fill loop
+loop = ml.pc
+ml.lda(0xffe)
+ml.sec()
+ml.adc(0)
+ml.sta(0xffe)
+ml.xor(4)
+ml.sta(0xffc)
+ml.lda(0xfff)
+ml.adc(0)
+ml.sta(0xfff)
+ml.xor(4)
+ml.sta(0xffd)
+ml.bcc(loop)
+
+ml.spc(0xff8)
+
+ml.set_pc(0xff8)
+ml.byte(1)
+
+stdout.buffer.write(ml.dump())
---
+++ circuits/simple2-cpu/example3.py
@@ -0,0 +1,38 @@
+from simple2_cpu_asm import *
+from sys import stdout
+
+ml = Memory()
+
+ml.set_pc(0x00)
+ml.byte(0xfe)
+ml.byte(0xd)
+ml.byte(0x12)
+ml.byte(0x5)
+
+ml.set_pc(0x10)
+ml.lda(0)
+loop = ml.pc
+ml.sec()
+ml.sbc(1)
+ml.bcc(ml.pc+5)
+ml.clc()
+ml.bcc(loop)
+ml.sta(0x100)
+
+loop = ml.pc
+ml.lda(0x200)
+ml.sec()
+ml.adc(0xf)
+ml.sta(0x200)
+ml.lda(2)
+ml.clc()
+ml.adc(3)
+ml.sta(2)
+ml.bcc(loop)
+
+ml.spc(0x80)
+
+ml.set_pc(0x80)
+ml.byte(1)
+
+stdout.buffer.write(ml.dump())
