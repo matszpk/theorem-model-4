ce506b7470d080394b9674571230a21c60ba40bf
matszpk <matszpk@interia.pl>
2023-03-14 14:34:55 +0100

    Write assembler stuff for simple2-cpu.

--- circuits/simple2-cpu/simple2_cpu_asm.py
+++ circuits/simple2-cpu/simple2_cpu_asm.py
@@ -11,22 +11,25 @@ instr_rol=8
 instr_ror=9
 instr_bcc=10
 instr_bne=11
-instr_bpl=12
-instr_jmp=13
-instr_psh=14
-instr_pul=15
+instr_bvc=12
+instr_bpl=13
+instr_spc=14
+instr_sec=15
+
+def instr_addr(addr):
+    return (((addr>>8)&0xf)<<4) | ((addr&0xff)<<8)
 
 class Memory:
     def __init__(self):
-        self.mem = [0]*256
+        self.mem = [0]*(1<<12)
         self.pc = 0
     
     def set_pc(self, pc):
-        self.pc = pc&0xff
+        self.pc = pc&0xfff
     
-    def nibble(self, a):
-        self.mem[self.pc] = a & 0xf
-        self.pc = (self.pc + 1) & 0xff
+    def byte(self, a):
+        self.mem[self.pc] = a & 0xff
+        self.pc = (self.pc + 1) & 0xfff
     
     def word16(self, a):
         self.byte(a&0xff)
@@ -35,51 +38,38 @@ class Memory:
     def word32(self, a):
         self.word16(a&0xffff)
         self.word16((a>>16)&0xffff)
-    
-    def byte(self, a):
-        self.nibble(a&0xf)
-        self.nibble((a>>4)&0xf)
-
-    def nibbles3(self, a, b, c):
-        self.nibble(a)
-        self.nibble(b)
-        self.nibble(c)
 
     def lda(self, addr):
-        self.nibbles3(instr_lda, addr&0xf, addr>>4)
+        self.word16(instr_lda | instr_addr(addr))
     def sta(self, addr):
-        self.nibbles3(instr_sta, addr&0xf, addr>>4)
+        self.word16(instr_sta | instr_addr(addr))
     def adc(self, addr):
-        self.nibbles3(instr_adc, addr&0xf, addr>>4)
+        self.word16(instr_adc | instr_addr(addr))
     def sbc(self, addr):
-        self.nibbles3(instr_sbc, addr&0xf, addr>>4)
-    def iand(self, addr):
-        self.nibbles3(instr_and, addr&0xf, addr>>4)
-    def ior(self, addr):
-        self.nibbles3(instr_or, addr&0xf, addr>>4)
+        self.word16(instr_sbc | instr_addr(addr))
+    def anda(self, addr):
+        self.word16(instr_and | instr_addr(addr))
+    def ora(self, addr):
+        self.word16(instr_or | instr_addr(addr))
     def xor(self, addr):
-        self.nibbles3(instr_xor, addr&0xf, addr>>4)
+        self.word16(instr_xor | instr_addr(addr))
     def clc(self):
-        self.nibble(instr_clc)
-    def rol(self):
-        self.nibble(instr_rol)
-    def ror(self):
-        self.nibble(instr_ror)
+        self.byte(instr_clc)
     def bcc(self, addr):
-        self.nibbles3(instr_bcc, addr&0xf, addr>>4)
+        self.word16(instr_bcc | instr_addr(addr))
     def bne(self, addr):
-        self.nibbles3(instr_bne, addr&0xf, addr>>4)
+        self.word16(instr_bne | instr_addr(addr))
+    def bvc(self, addr):
+        self.word16(instr_bvc | instr_addr(addr))
     def bpl(self, addr):
-        self.nibbles3(instr_bpl, addr&0xf, addr>>4)
-    def jmp(self, addr):
-        self.nibbles3(instr_jmp, addr&0xf, addr>>4)
-    def psh(self):
-        self.nibble(instr_psh)
-    def pul(self):
-        self.nibble(instr_pul)
+        self.word16(instr_bpl | instr_addr(addr))
+    def spc(self, addr):
+        self.word16(instr_spc | instr_addr(addr))
+    def sec(self, addr):
+        self.byte(instr_sec)
 
     def dump(self):
         out = b''
-        for i in range(0,128):
-            out += bytes([self.mem[i*2] + (self.mem[i*2+1]<<4)])
+        for i in range(0,1<<12):
+            out += bytes(self.mem)
         return out
