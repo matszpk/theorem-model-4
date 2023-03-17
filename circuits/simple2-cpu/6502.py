666ba5e11da19ceb6250bdaf56955dc4a8fe16f5
matszpk <matszpk@interia.pl>
2023-03-17 08:48:38 +0100

    Some new stuff for 6502.

--- circuits/simple2-cpu/6502.py
+++ circuits/simple2-cpu/6502.py
@@ -3,105 +3,98 @@ from sys import stdout
 
 ml = Memory()
 
-# handling calls:
-# lda call_descriptor -> call address, return address, should be nonzero
-# bne call_handler
-#
-# # lda call_descriptor -> call address, return address, should be nonzero
-# bne subroutine
-#
-# subroutine:
-# bne call_handler
+ml.set_pc(0xff0)
+npc = ml.pc # 0xff0: program counter
+ml.word16(0x0000, [True,True])
+nsr = ml.pc # 0xff2: processor status
+ml.byte(0x00, True)
+nsp = ml.pc # 0xff3: stack pointer
+ml.byte(0xff, True)
+nacc = ml.pc # 0xff4:
+ml.byte(0x00, True)
+nxind = ml.pc # 0xff5:
+ml.byte(0x00, True)
+nyind = ml.pc # 0xff6:
+ml.byte(0x00, True)
+nopcode = ml.pc # 0xff7:
+ml.byte(0x00, True)
+narglo = ml.pc # 0xff8:
+ml.byte(0x00, True)
+narghi = ml.pc # 0xff9:
+ml.byte(0x00, True)
+temp1 = ml.pc # 0xffa:
+ml.byte(0x00, True)
+temp2 = ml.pc # 0xffb:
+ml.byte(0x00, True)
 
-# 16-element table and LDA, XOR combinations to get 256 values???
-# no: use loading from memory. extra 2 bytes added by extra instructions
-#
-main_prog = 0
-ee_jump = 0
-for p in range(0,2):
+load_inc_pc, load_inc_pc_ch = -10000, -10000
+
+def call_proc_8b(proc):
+    if proc < 0:
+        ml.lda_imm(1) # ret address
+        ml.bne(proc)
+        return
+    page = proc&0xf00
+    addr = ml.pc+4
+    if addr > 0+page and addr < 0x100+page:
+        ml.lda_imm(ml.pc+4) # ret address
+        ml.bne(proc)
+        return addr
+    else:
+        raise(RuntimeError("Address above range!"))
+
+def gencode():
+    global load_inc_pc, load_inc_pc_ch
+    
     start = 0
-    ml.byte(0x0)
-    ml.byte(0x10)
-    ml.byte(0x30)
-    ml.byte(0x0)
-    npc = ml.pc # program counter
-    ml.word16(0x0000)
-    nsr = ml.pc # processor status
-    ml.byte(0x00)
-    nsp = ml.pc # stack pointer
-    ml.byte(0xff)
-    nacc = ml.pc
-    ml.byte(0x00)
-    nxind = ml.pc
-    ml.byte(0x00)
-    nyind = ml.pc
-    ml.byte(0x00)
-    nopcode = ml.pc
-    ml.byte(0x00)
-    narglo = ml.pc
-    ml.byte(0x00)
-    narghi = ml.pc
-    ml.byte(0x00)
-    call_exec_p = ml.pc
-    ml.byte(0x00)
-
-    start = (ml.pc + 15) & 16
     ml.set_pc(start)
-    one = ml.pc+2
     # create 6502 machine - memory: 16-bit address, 8-bit cell
-    for i in range(0,3):
-        ml.lda(0x1+i)
-        ml.sta(0xffd+i)
+    ml.lda_imm(0x10)
+    ml.sta(0xffd)
+    ml.lda_imm(0x30)
+    ml.sta(0xffe)
+    ml.lda_imm(0)
+    ml.sta(0xfff)
     ml.spc(0) # call it
 
     main_loop = ml.pc
-    # load instruction from pc
-    ml.lda(npc)
-    ml.sta(0xffe)
-    ml.sec()
-    ml.adc(0x0)
-    ml.sta(npc)
-    ml.sta(0xffe)
-    ml.lda(npc+1)
-    ml.adc(0x0)
-    ml.sta(npc+1)
-    ml.lda(0xffd)
-    # opcode
-    ml.sta(nopcode)
-    # decode instruction
+    # load opcode
+    call_proc_8b(load_inc_pc)
+    # decode it
     
-
-    ml.lda(npc)
-    ml.sta(0xffe)
-    ml.sec()
-    ml.adc(0x0)
-    ml.sta(npc)
-    ml.sta(0xffe)
-    ml.lda(npc+1)
-    ml.adc(0x0)
-    ml.sta(npc+1)
-    ml.lda(0xffd)
-    ml.sta(narglo)
-
-    ml.lda(npc)
-    ml.sta(0xffe)
-    ml.sec()
-    ml.adc(0x0)
-    ml.sta(npc)
-    ml.sta(0xffe)
-    ml.lda(npc+1)
-    ml.adc(0x0)
-    ml.sta(npc+1)
-    ml.lda(0xffd)
-    ml.sta(narghi)
-
-    ml.spc(one)
+    # load argument low
+    call_proc_8b(load_inc_pc)
+    
+    # load argument high
+    call_proc_8b(load_inc_pc)
+    
+    ml.clc()
+    ml.bcc(main_loop)
     
     # main_prog subprogs
+    
+    # load byte from pc and increment pc
+    load_inc_pc = ml.pc
+    ml.sta(load_inc_pc_ch+1)
+    ml.lda(npc)
+    ml.sta(0xffe)
+    ml.sec()
+    ml.adc_imm(0)
+    ml.sta(npc)
+    ml.lda(npc+1)
+    ml.sta(0xfff)
+    ml.adc_imm(0)
+    ml.sta(npc+1)
+    ml.lda(0xffd)
+    ml.clc()
+    load_inc_pc_ch = ml.pc
+    ml.bcc(0, [False, True])
     
     # tables:
     # addressing modes:
     # 
-    
+    return start
+
+ml.assemble(gencode)
 
 stdout.buffer.write(ml.dump())
