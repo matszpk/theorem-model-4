ffcf70e398cb92386fff94f24b8939935ad79b26
matszpk <matszpk@interia.pl>
2023-03-20 21:24:19 +0100

    Write first automated testcases for 6502.

---
+++ circuits/simple2-cpu/6502-testsuite.py
@@ -0,0 +1,110 @@
+#######
+# 6502 testsuite
+#######
+
+import os
+import shutil
+import subprocess
+
+THEOREM_MODEL = './target/release/theorem-model-4'
+CIRCUIT = 'circuits/simple2-cpu/simple2-cpu.circuit'
+CG6502 = 'circuits/simple2-cpu/6502.py'
+PYTHON = '/usr/bin/python'
+
+pc_offset = 0xfe0
+sr_offset = 0xfe2
+sp_offset = 0xfe3
+acc_offset = 0xfe4
+xind_offset = 0xfe5
+yind_offset = 0xfe6
+
+def cleanup():
+    for r in ['6502mem.raw', '6502membase.raw', '6502progbase.raw',
+                '6502prog.raw', '6502memdump.raw', '6502progdump.raw']:
+        try:
+            os.remove(r)
+        except:
+            pass
+        finally:
+            pass
+
+cleanup()
+
+def run_testcase(name, exp_list, mem_list, pc=0, acc=0, xind=0, yind=0, sp=0xff, sr=0):
+    # prepare test
+    shutil.copyfile('6502progbase.raw', '6502prog.raw')
+    with open('6502prog.raw','rb+') as f:
+        f.seek(pc_offset, 0)
+        f.write(bytes([pc&0xff, (pc>>8)&0xff, sr&0xff, sp&0xff, acc&0xff, xind&0xff, yind&0xff]))
+    shutil.copyfile('6502membase.raw', '6502mem.raw')
+    with open('6502mem.raw','rb+') as f:
+        for (offset, bts) in mem_list:
+            f.seek(offset, 0)
+            f.write(bytes([x&0xff for  x in bts]))
+    # run command
+    cp = subprocess.run([THEOREM_MODEL, 'run-machine', CIRCUIT, '3', '0', '6502mem.raw',
+                '6502prog.raw', '-f', '6502memdump.raw,6502progdump.raw'], capture_output=True)
+    if cp.returncode != 0:
+        print("State:", cp)
+        raise(RuntimeError('{}: Error while running testcase'.format(name)))
+    # get outputs
+    memdump=b''
+    progdump=b''
+    with open('6502memdump.raw','rb') as f:
+        memdump = f.read()
+    with open('6502progdump.raw','rb') as f:
+        progdump = f.read()
+    mems = [memdump,progdump]
+    passed = True
+    for (m,pos,exp) in exp_list:
+        if mems[m][pos] != exp:
+            posname = '{} {}'.format(m, pos)
+            if m == 1:
+                if pos==pc_offset:
+                    posname = 'pclo'
+                elif pos==pc_offset+1:
+                    posname = 'pchi'
+                elif pos==sr_offset:
+                    posname='sr'
+                elif pos==sp_offset:
+                    posname='sp'
+                elif pos==acc_offset:
+                    posname='acc'
+                elif pos==xind_offset:
+                    posname='xind'
+                elif pos==yind_offset:
+                    posname='yind'
+            print('TEST {}: FAILURE: {}: {}!={}'.format(name, posname, exp, mems[m][pos]))
+            passed = False
+    if passed:
+        print("TEST", name, "PASSED")
+
+try:
+    ###################
+    cp = subprocess.run([PYTHON, CG6502], capture_output=True)
+    if cp.returncode != 0:
+        raise(RuntimeError('Error while running code generation of 6502'))
+    with open('6502progbase.raw','wb') as f:
+        f.write(cp.stdout)
+
+    with open('6502membase.raw','wb') as f:
+        f.write(b'\x00'*(1<<16))
+    
+    ####################
+    # main tests
+    
+    def test_clc(sr):
+        run_testcase('clc sr={}'.format(sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, sr & (0xff ^ 0x1)),
+            ],
+            [ (0x200, [0x18, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, acc=0, sr=sr)
+    
+    for i in range(0,256):
+        test_clc(i)
+    
+finally:
+    cleanup()
