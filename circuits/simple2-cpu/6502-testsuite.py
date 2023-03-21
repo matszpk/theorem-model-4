cb9429764a243c9159bab3e2e6664c6057fe0eee
matszpk <matszpk@interia.pl>
2023-03-21 14:14:28 +0100

    Add next testcases for 6502-testsuite. Add summary to 6502-testsuite.

--- circuits/simple2-cpu/6502-testsuite.py
+++ circuits/simple2-cpu/6502-testsuite.py
@@ -22,7 +22,12 @@ xind_offset = 0xfe5
 yind_offset = 0xfe6
 instr_cycles_offset = 0xfe7
 
+tests_passed = 0
+tests_failed = 0
+
 def cleanup():
+    tests_passed = 0
+    tests_failed = 0
     for r in ['6502mem.raw', '6502membase.raw', '6502progbase.raw',
                 '6502prog.raw', '6502memdump.raw', '6502progdump.raw']:
         try:
@@ -35,6 +40,7 @@ def cleanup():
 cleanup()
 
 def run_testcase(name, exp_list, mem_list, pc=0, acc=0, xind=0, yind=0, sp=0xff, sr=0):
+    global tests_passed, tests_failed
     # prepare test
     shutil.copyfile('6502progbase.raw', '6502prog.raw')
     with open('6502prog.raw','rb+') as f:
@@ -79,8 +85,10 @@ def run_testcase(name, exp_list, mem_list, pc=0, acc=0, xind=0, yind=0, sp=0xff,
                 elif pos==yind_offset:
                     posname='yind'
             print('TEST {}: FAILURE: {}: {}!={}'.format(name, posname, exp, mems[m][pos]))
+            tests_failed += 1
             passed = False
     if passed:
+        tests_passed += 1
         print("TEST", name, "PASSED")
 
 try:
@@ -127,6 +135,81 @@ try:
             [ (0x200, [0xaa, 0x04]) ],   # instructions. last is undefined (stop)
             pc=0x200, acc=acc, sr=sr, xind=1 if acc==0 else 0)
     
+    def test_txa(xind, sr):
+        run_testcase('txa xind={} sr={}'.format(xind, sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, (sr&(0xff^2^128)) | (0 if xind!=0 else 2) | (xind&0x80)),
+                (1, acc_offset, xind),
+                (1, xind_offset, xind),
+                (1, yind_offset, 0),
+                (1, sp_offset, 0xff),
+                (1, instr_cycles_offset, 2),
+            ],
+            [ (0x200, [0x8a, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, acc=1 if xind==0 else 0, sr=sr, xind=xind)
+    
+    def test_tay(acc, sr):
+        run_testcase('tay acc={} sr={}'.format(acc, sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, (sr&(0xff^2^128)) | (0 if acc!=0 else 2) | (acc&0x80)),
+                (1, acc_offset, acc),
+                (1, xind_offset, 0),
+                (1, yind_offset, acc),
+                (1, sp_offset, 0xff),
+                (1, instr_cycles_offset, 2),
+            ],
+            [ (0x200, [0xa8, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, acc=acc, sr=sr, yind=1 if acc==0 else 0)
+    
+    def test_tya(yind, sr):
+        run_testcase('tya yind={} sr={}'.format(yind, sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, (sr&(0xff^2^128)) | (0 if yind!=0 else 2) | (yind&0x80)),
+                (1, acc_offset, yind),
+                (1, xind_offset, 0),
+                (1, yind_offset, yind),
+                (1, sp_offset, 0xff),
+                (1, instr_cycles_offset, 2),
+            ],
+            [ (0x200, [0x98, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, acc=1 if yind==0 else 0, sr=sr, yind=yind)
+    
+    def test_tsx(sp, sr):
+        run_testcase('tsx sp={} sr={}'.format(sp, sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, (sr&(0xff^2^128)) | (0 if sp!=0 else 2) | (sp&0x80)),
+                (1, acc_offset, 0),
+                (1, xind_offset, sp),
+                (1, yind_offset, 0),
+                (1, sp_offset, sp),
+                (1, instr_cycles_offset, 2),
+            ],
+            [ (0x200, [0xba, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, sp=sp, sr=sr, xind=1 if sp==0 else 0)
+    
+    def test_txs(xind, sr):
+        run_testcase('txs xind={} sr={}'.format(xind, sr),
+            [
+                (1, pc_offset, (0x202)&0xff),
+                (1, pc_offset+1, ((0x202)>>8)&0xff),
+                (1, sr_offset, sr),
+                (1, acc_offset, 0),
+                (1, xind_offset, xind),
+                (1, yind_offset, 0),
+                (1, sp_offset, xind),
+                (1, instr_cycles_offset, 2),
+            ],
+            [ (0x200, [0x9a, 0x04]) ],   # instructions. last is undefined (stop)
+            pc=0x200, sp=1 if xind==0 else 0, sr=sr, xind=xind)
+    
     def test_clc(sr): test_chsr('clc', 0x18, 0, True, sr)
     def test_cld(sr): test_chsr('cld', 0xd8, 3, True, sr)
     def test_cli(sr): test_chsr('cli', 0x58, 2, True, sr)
@@ -151,6 +234,32 @@ try:
         test_tax(i, 0x91)
         test_tax(i, 0x13)
         test_tax(i, 0x93)
+        test_txa(i, 0x11)
+        test_txa(i, 0x91)
+        test_txa(i, 0x13)
+        test_txa(i, 0x93)
+        
+        test_tay(i, 0x11)
+        test_tay(i, 0x91)
+        test_tay(i, 0x13)
+        test_tay(i, 0x93)
+        test_tya(i, 0x11)
+        test_tya(i, 0x91)
+        test_tya(i, 0x13)
+        test_tya(i, 0x93)
+        
+        test_tsx(i, 0x11)
+        test_tsx(i, 0x91)
+        test_tsx(i, 0x13)
+        test_tsx(i, 0x93)
+        test_txs(i, 0x11)
+        test_txs(i, 0x91)
+        test_txs(i, 0x13)
+        test_txs(i, 0x93)
     
+    #########################
+    # Summary
+    print("Tests passed: {}, Tests failed: {}, Total: {}" \
+            .format(tests_passed, tests_failed, tests_passed+tests_failed))
 finally:
     cleanup()
